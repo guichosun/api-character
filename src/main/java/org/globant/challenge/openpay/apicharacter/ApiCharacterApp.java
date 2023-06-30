2522429c463cf51139d69f48ae4dc681b8a85be9
Luis Alberto <luis.delcampo@globant.com>
2023-06-30 09:28:41 -0600

    Enabled feign clients.

--- src/main/java/org/globant/challenge/openpay/apicharacter/ApiCharacterApp.java
+++ src/main/java/org/globant/challenge/openpay/apicharacter/ApiCharacterApp.java
@@ -2,13 +2,15 @@ package org.globant.challenge.openpay.apicharacter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
- * Main class for the Spring-boot application,
- *
+ * Main class for the Spring-boot application.
+ * Enable Feign Clients
  * @author luis.delcampo
  */
 @SpringBootApplication
+@EnableFeignClients
 public class ApiCharacterApp {
 
     public static void main(String[] args) {
