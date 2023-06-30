d516546867d414f0898bca0725d79497de36a6b8
Luis Alberto <luis.delcampo@globant.com>
2023-06-30 16:54:23 -0600

    Add a simple test that checks context load

---
+++ src/test/java/com/globant/challenge/openpay/apicharacter/ApiCharacterAppTests.java
@@ -0,0 +1,13 @@
+package com.globant.challenge.openpay.apicharacter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCharacterAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
